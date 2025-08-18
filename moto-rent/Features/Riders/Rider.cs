9fae385aaf7455266bf6defbb8ccf5a6f0032e36
Eric Arnou Santos <ericarnou68@gmail.com>
2025-08-18 18:51:05 -0300

    fix: removed Email field and added BirthDate

--- moto-rent/Features/Riders/Rider.cs
+++ moto-rent/Features/Riders/Rider.cs
@@ -4,7 +4,7 @@ public class Rider
     public int Id { get; set; }
     public string Name { get; set; } = string.Empty;
     public string Cnpj { get; set; } = string.Empty;
-
+    public DateTime BirthDate { get; set; }
     public string Cnh { get; set; } = string.Empty;
 
     public enum CnhCategory
@@ -13,5 +13,4 @@ public class Rider
         B,
         AB
     }
-    public string Email { get; set; } = string.Empty;
 }
