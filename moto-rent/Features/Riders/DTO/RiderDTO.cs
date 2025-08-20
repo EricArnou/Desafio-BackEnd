dbf441c96349e6b3f6c3ae60ee06df9055af989d
Eric Arnou Santos <ericarnou68@gmail.com>
2025-08-20 00:18:27 -0300

    refactor: rename RiderDto attributes

--- moto-rent/Features/Riders/DTO/RiderDTO.cs
+++ moto-rent/Features/Riders/DTO/RiderDTO.cs
@@ -8,19 +8,19 @@ namespace moto_rent.Features.Riders.DTOs
             identificador = rider.Id;
             nome = rider.Name;
             cnpj = rider.Cnpj;
-            dataNascimento = rider.BirthDate;
-            cnh = rider.Cnh;
-            CnhCategory = rider.CnhCategory.ToString();
-            imagemCnh = rider.ImageCnh;
+            data_nascimento = rider.BirthDate;
+            numero_cnh = rider.Cnh;
+            tipo_cnh = rider.CnhCategory.ToString();
+            imagem_cnh = rider.ImageCnh;
         }
 
         public RiderDto() { }
         public string identificador { get; set; } = Guid.NewGuid().ToString("N");
         public string nome { get; set; } = string.Empty;
         public string cnpj { get; set; } = string.Empty;
-        public DateTime dataNascimento { get; set; }
-        public string cnh { get; set; } = string.Empty;
-        public string CnhCategory { get; set; } = string.Empty;
-        public string imagemCnh { get; set; } = string.Empty;
+        public DateTime data_nascimento { get; set; }
+        public string numero_cnh { get; set; } = string.Empty;
+        public string tipo_cnh { get; set; } = string.Empty;
+        public string imagem_cnh { get; set; } = string.Empty;
     }
 }
